0d1c62e7c3066f486ed47f8cc4c6b1885862634a
AmirSohail <43651743+Amirsohail335@users.noreply.github.com>
2022-03-02 23:12:44 +0530

    Create Qicksort_code_studio.cpp

---
+++ Qicksort_code_studio.cpp
@@ -0,0 +1,57 @@
+
+int partition(vector<int> & arr, int s, int e){
+    int pivot = arr[s];
+
+    int cnt =0;
+    for(int i=s+1; i<=e; i++){
+        if(arr[i] <= pivot)
+            cnt++;
+    }
+
+    // place pivot at right place
+    int pivotIndex = s+cnt;
+    swap(arr[pivotIndex], arr[s]);
+
+    //left and right parts
+    int i = s, j = e;
+    while( i < pivotIndex && j > pivotIndex){
+
+        while(arr[i] < pivot){
+            i++;
+        }
+        while (arr[j] > pivot)
+        {
+            j--;
+        }
+        if(i <pivotIndex && j>pivotIndex){
+            swap(arr[i++], arr[j--]);
+        
+        }
+        
+    }
+
+}
+
+void solve(vector<int> & arr, int s, int e){
+
+    //base case
+    if(s>=e)
+        return;
+
+    //partition
+    int p = partition(arr , s, e);
+
+    //sorting left part
+    solve(arr, s, p-1);
+
+    //sorting right part
+    solve(arr, p+1, e);
+
+}
+
+vector<int> quickSort(vector<int> arr)
+{
+    // Write your code here.
+    solve(arr, 0, arr.size()-1);
+    return arr;
+}
